98a88b78419e83ce4603bbbdcc3c1cea475cda8e
Patrik Gustavsson <patrik_jag@hotmail.se>
2024-05-07 14:02:03 +0200

    Addsyncfusion

--- ClothingEcommerceClient/Program.cs
+++ ClothingEcommerceClient/Program.cs
@@ -3,6 +3,7 @@ using ClothingEcommerceClient.Services;
 using ClothingEcommerceSharedLibrary.Contracts;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,4 +11,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProduct, ClientServices>();
+builder.Services.AddSyncfusionBlazor();
 await builder.Build().RunAsync();
