4fdef6109f5a14d361ed15e9a61d38f0e3ed08fd
Patrik Gustavsson <patrik_jag@hotmail.se>
2024-05-21 21:26:20 +0200

    buying cart

--- ClothingEcommerceClient/Program.cs
+++ ClothingEcommerceClient/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<IProductService, ClientServices>();
 builder.Services.AddScoped<ICategoryService, ClientServices>();
 builder.Services.AddScoped<IUserAccountService, ClientServices>();
+builder.Services.AddScoped<ICart, ClientServices>();
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped <AuthenticationService>();
